2f51e50088d96876e33958e976f38cfabe0b53de
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-14 15:49:41 +0530

    ✅ Max Consecutive Ones | Easy | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-07-14

--- leetcode/easy/0485-max-consecutive-ones/solution.py
+++ leetcode/easy/0485-max-consecutive-ones/solution.py
@@ -6,7 +6,11 @@ class Solution:
         max_count = 0
         
         for num in nums:
-            count = count + 1 if num == 1 else 0
-            max_count = max(max_count, count)
+            if num == 1:
+                count += 1
+            else:  
+                max_count = max(max_count, count)  
+                count = 0  
+        max_count = max(max_count, count)
         
         return max_count

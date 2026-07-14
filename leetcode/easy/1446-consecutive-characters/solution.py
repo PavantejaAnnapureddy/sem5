188880b4d22a8082fb003684329f597707ab3d05
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-14 15:55:09 +0530

    ✅ Consecutive Characters | Easy | Python | Runtime: 2 ms (85.79%) | Memory: 19.3 MB | 2026-07-14

--- leetcode/easy/1446-consecutive-characters/solution.py
+++ leetcode/easy/1446-consecutive-characters/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        max_count = current_count = 1
+        count = 1  
+        max_count = 1
         
         for i in range(1, len(s)):
             if s[i] == s[i-1]:
-                current_count += 1
-                max_count = max(max_count, current_count)
-            else:
-                current_count = 1
-        
+                count += 1
+            else: 
+                max_count = max(max_count, count)
+                count = 1  
+        max_count = max(max_count, count)
         return max_count

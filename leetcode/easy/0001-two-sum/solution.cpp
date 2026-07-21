b0d99b2074a57c1587b7837d50cd92428948d986
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-21 15:56:09 +0530

    ✅ Two Sum | Easy | C++ | Runtime: 2 ms (72.22%) | Memory: 16.5 MB | 2026-07-21

--- leetcode/easy/0001-two-sum/solution.cpp
+++ leetcode/easy/0001-two-sum/solution.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Create complement array
+        vector<int> complements(nums.size());
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+            complements[i] = target - nums[i];
+        }
+        
+        // Now we need to find if any nums[i] equals any complements[j]
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            numMap[nums[i]] = i;
+        }
+        
+        for (int j = 0; j < complements.size(); j++) {
+            if (numMap.find(complements[j]) != numMap.end() && numMap[complements[j]] != j) {
+                return {j, numMap[complements[j]]};
             }
         }
         return {};

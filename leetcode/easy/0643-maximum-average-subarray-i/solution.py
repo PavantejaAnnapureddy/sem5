a854fb6133538a1c21471d58c5f246c6b3478e67
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-28 15:18:29 +0530

    ✅ Maximum Average Subarray I | Easy | Python | Runtime: 55 ms (73.76%) | Memory: 28.9 MB | 2026-07-28

--- leetcode/easy/0643-maximum-average-subarray-i/solution.py
+++ leetcode/easy/0643-maximum-average-subarray-i/solution.py
@@ -1,14 +1,9 @@
-from typing import List
-
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        curr_sum = sum(nums[:k])
-        max_sum = curr_sum
-        
-
+        s = sum(nums[:k])
+        m = s
         for i in range(k, len(nums)):
-            curr_sum += nums[i] - nums[i - k]
-            max_sum = max(max_sum, curr_sum)
-        
-        return max_sum / k
+            s += nums[i] - nums[i-k]
+            m = max(m, s)
+        return m / k
         

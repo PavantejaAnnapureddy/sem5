e5e151cc0662c1317720206d24a35cc264af64c2
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-03 20:51:07 +0530

    ✅ CLSC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/CLSC/solution.py
+++ codechef/medium/CLSC/solution.py
@@ -1,4 +1,4 @@
-N=int(input().split())
-arr=map(int,input().split())
-miinus= min(arr[i]-arr[i-1]) for i range (1,N)
+N=int(input())
+arr= sorted(map(int,input().split()))
+miinus= min(arr[i]-arr[i-1] for i  in range(1, N))
 print(miinus)

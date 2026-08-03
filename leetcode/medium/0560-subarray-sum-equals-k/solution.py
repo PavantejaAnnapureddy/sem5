91c248db40bc8406517e04251a3392d77ce6d4aa
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-03 14:11:36 +0530

    ✅ Subarray Sum Equals K | Medium | Python | Runtime: 28 ms (83.34%) | Memory: 21.7 MB | 2026-08-03

---
+++ leetcode/medium/0560-subarray-sum-equals-k/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        prefix_sum = 0
+        prefix_freq = {0: 1}
+        
+        for num in nums:
+            prefix_sum += num
+            
+            if (prefix_sum - k) in prefix_freq:
+                count += prefix_freq[prefix_sum - k]
+            
+            prefix_freq[prefix_sum] = prefix_freq.get(prefix_sum, 0) + 1
+        
+        return count

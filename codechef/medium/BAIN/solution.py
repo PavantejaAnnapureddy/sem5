d71953a0c8f552fd41c8f3e1c16822d26ebfe34b
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-03 20:43:46 +0530

    ✅ BAIN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/BAIN/solution.py
+++ codechef/medium/BAIN/solution.py
@@ -1,2 +1,13 @@
-a,b,c,d=map(int,input().split())
 
+from itertools import combinations
+
+a, b, c, d = map(int, input().split())
+arr = [a, b, c, d]
+
+for r in range(1, 5):  
+    for combo in combinations(arr, r):
+        if sum(combo) == 0:
+            print("Yes")
+            exit()
+
+print("No")

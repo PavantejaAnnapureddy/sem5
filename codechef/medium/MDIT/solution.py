95bef16ec74d7058b854bc7254a3aa11a7fd6cd8
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-03 21:00:47 +0530

    ✅ MDIT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/MDIT/solution.py
+++ codechef/medium/MDIT/solution.py
@@ -1,8 +1,25 @@
-
+n = int(input())
+v = list(map(int, input().split()))
+p = list(map(int, input().split())
+children = [[] for _ in range(n)]
 root = -1
-for i in range(n):
-    if parent[i] == -1:
-        root = i
-        break
 
-root = parent.index(-1)  #
+for i in range(n):
+    if p[i] == -1:
+        root = i
+    else:
+        children[p[i]-1].append(i)
+ans = 0
+stack = [(root, v[root], v[root])]
+
+while stack:
+    node, mn, mx = stack.pop()
+    ans = max(ans, abs(v[node]-mn), abs(v[node]-mx))
+    
+    new_mn = min(mn, v[node])
+    new_mx = max(mx, v[node])
+    
+    for child in children[node]:
+        stack.append((child, new_mn, new_mx))
+
+print(ans)

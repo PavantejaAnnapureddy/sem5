cb2333841dc654c477814368c0a2d6b0a6a86b36
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-05 20:15:58 +0530

    ✅ BISHOPMV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/BISHOPMV/solution.py
+++ codechef/medium/BISHOPMV/solution.py
@@ -1,1 +1,15 @@
-# cook your dish here
+
+T = int(input())
+
+for _ in range(T):
+    X1, Y1, X2, Y2 = map(int, input().split())
+    if X1 == X2 and Y1 == Y2:
+        print(0)
+        continue
+    if abs(X1 - X2) == abs(Y1 - Y2):
+        print(1)
+        continue
+    if (X1 + Y1) % 2 == (X2 + Y2) % 2:
+        print(2)
+    else:
+        print(-1)

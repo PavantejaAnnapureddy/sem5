0d426e0e974ce9a5885103dfe3a49db013ac7054
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-10 20:14:20 +0530

    ✅ MXLED | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-10

--- codechef/medium/MXLED/solution.py
+++ codechef/medium/MXLED/solution.py
@@ -1,1 +1,15 @@
-# cook your dish here
+n = int(input())
+
+p1 = p2 = max_lead = 0
+winner = 1
+
+for _ in range(n):
+    s, t = map(int, input().split())
+    p1 += s
+    p2 += t
+    
+    if abs(p1 - p2) > max_lead:
+        max_lead = abs(p1 - p2)
+        winner = 1 if p1 > p2 else 2
+
+print(winner, max_lead)

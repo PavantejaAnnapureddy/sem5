615590b251529d1c508c954a6b836d5b7a7488f8
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-10 20:25:36 +0530

    ✅ RPTS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-10

--- codechef/medium/RPTS/solution.py
+++ codechef/medium/RPTS/solution.py
@@ -1,13 +1,14 @@
 n, k = map(int, input().split())
 s = input().strip()
 
-min_repaint = float('inf')
+whites = s[:k].count('W')
+min_repaint = whites
 
-for i in range(n - k + 1):
-    whites = 0
-    for j in range(i, i + k):
-        if s[j] == 'W':
-            whites += 1
+for i in range(n - k):
+    if s[i] == 'W':
+        whites -= 1
+    if s[i + k] == 'W':
+        whites += 1
     min_repaint = min(min_repaint, whites)
 
 print(min_repaint)

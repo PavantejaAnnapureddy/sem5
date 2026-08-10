d5b817f35df5185f16d1cfde5991471f2a2d0b74
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-10 20:33:21 +0530

    ✅ MXWC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-10

--- codechef/medium/MXWC/solution.py
+++ codechef/medium/MXWC/solution.py
@@ -11,10 +11,7 @@ while left < right:
     min_height = min(height[left], height[right])
     water = width * min_height
     
-    # Update maximum water
     max_water = max(max_water, water)
-    
-    # Move the pointer pointing to the shorter line
     if height[left] < height[right]:
         left += 1
     else:

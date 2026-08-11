9a6912523250299e42787447b0c87a2ffcdfff40
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-11 14:25:25 +0530

    ✅ Shortest Unsorted Continuous Subarray | Medium | Python | Runtime: 6 ms (85.41%) | Memory: 20.5 MB | 2026-08-11

---
+++ leetcode/medium/0581-shortest-unsorted-continuous-subarray/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findUnsortedSubarray(self, nums: list[int]) -> int:
+        n = len(nums)
+        left, right = -1, -2 
+        
+        max_seen = float('-inf')
+        for i in range(n):
+            if nums[i] < max_seen:
+                right = i
+            else:
+                max_seen = nums[i]
+        
+        min_seen = float('inf')
+        for i in range(n - 1, -1, -1):
+            if nums[i] > min_seen:
+                left = i
+            else:
+                min_seen = nums[i]
+        
+        return right - left + 1

65d4c22c793ca86ae906bd9d2cb97d6c9d4032c8
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-12 21:04:28 +0530

    ✅ CHOCGM | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHOCGM/solution.py
+++ codechef/medium/CHOCGM/solution.py
@@ -1,4 +1,4 @@
-T = int(input())
+"""T = int(input())
 for _ in range(T):
     N = int(input())
     A = list(map(int, input().split()))
@@ -38,5 +38,36 @@ for _ in range(T):
                         A[i] = 0
                         total -= 1
                         break
+    
+    print(alice)"""
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    
+    total = sum(A)
+    alice = 0
+    
+    while total > 0:
+        if total % 2 == 0:
+            max_idx = max(range(N), key=lambda i: A[i])
+            
+            if A[max_idx] >= 2:
+                A[max_idx] -= 2
+                alice += 2
+                total -= 2
+            else:
+                A[max_idx] -= 1
+                alice += 1
+                total -= 1
+        else:
+            max_idx = max(range(N), key=lambda i: A[i])
+            
+            if A[max_idx] >= 2:
+                A[max_idx] -= 2
+                total -= 2
+            else:
+                A[max_idx] -= 1
+                total -= 1
     
     print(alice)

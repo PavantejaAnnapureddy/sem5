d3e6acb8318f673fd49b63b7c8f574565643747d
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-12 20:12:43 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -2,4 +2,23 @@
 T=int(input())
 for_ in range(T):
     N=int(input())
-    
+    ans = float('inf')
+    curr = N 
+    ops = 0
+    while curr%3 !=0:
+        curr +=1
+        ops +=1
+    ans = min(ans, ops)
+    curr = N 
+    ops=0
+    if curr %5 !=0:
+        ops +=1
+        curr =((curr // 5)+1)*5
+    else:
+        ops +=1
+        curr+= 5
+    while curr %3 !=0:
+        curr +=1
+        ops+=1
+    ans = min(ans, ops)
+    print(ans)

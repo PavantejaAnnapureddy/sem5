2d145a0cc43c5a61f751be7a81774709411f775c
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-12 20:32:06 +0530

    ✅ CHOCGM | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHOCGM/solution.py
+++ codechef/medium/CHOCGM/solution.py
@@ -1,2 +1,43 @@
-# cook your dish here
-N=int(input())
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    
+    total = sum(A)
+    alice = 0
+    
+    while total > 0:
+        if total % 2 == 0:
+            ate = False
+            for i in range(N):
+                if A[i] >= 2:
+                    A[i] -= 2
+                    alice += 2
+                    total -= 2
+                    ate = True
+                    break
+            if not ate:
+                # Must eat 1
+                for i in range(N):
+                    if A[i] == 1:
+                        A[i] = 0
+                        alice += 1
+                        total -= 1
+                        break
+        else:
+            ate = False
+            for i in range(N):
+                if A[i] >= 2:
+                    A[i] -= 2
+                    total -= 2
+                    ate = True
+                    break
+            if not ate:
+           
+                for i in range(N):
+                    if A[i] == 1:
+                        A[i] = 0
+                        total -= 1
+                        break
+    
+    print(alice)

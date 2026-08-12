39722574fd632347bcecf3001ec125a2c7b22c03
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-12 20:18:59 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -22,3 +22,29 @@ for _ in range(T):
         ops+=1
     ans = min(ans, ops)
     print(ans)
+"""T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    
+    # Strategy 1: Only add 1s
+    if N % 3 == 0:
+        ans = 0
+    elif N % 3 == 1:
+        ans = 2
+    else:  # N % 3 == 2
+        ans = 1
+    
+   
+    next_multiple_of_5 = ((N // 5) + 1) * 5
+    
+    ops = 1  # for the jump
+    if next_multiple_of_5 % 3 == 0:
+        ops += 0
+    elif next_multiple_of_5 % 3 == 1:
+        ops += 2
+    else:  # next_multiple_of_5 % 3 == 2
+        ops += 1
+    
+    ans = min(ans, ops)
+    print(ans)

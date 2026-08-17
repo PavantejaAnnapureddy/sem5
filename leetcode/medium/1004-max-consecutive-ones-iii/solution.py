799bc95b62a841249704a30c85b127a538b36d70
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-17 11:35:55 +0530

    ✅ Max Consecutive Ones III | Medium | Python | Runtime: 49 ms (87.33%) | Memory: 22 MB | 2026-08-17

--- leetcode/medium/1004-max-consecutive-ones-iii/solution.py
+++ leetcode/medium/1004-max-consecutive-ones-iii/solution.py
@@ -16,21 +16,3 @@ class Solution:
             max_len = max(max_len, right - left + 1)
         
         return max_len
-#from collections import Counter
-"""
-#class Solution:
-    def longestOnes(self, nums: list[int], k: int) -> int:
-        window_count = Counter()  
-        left = 0
-        max_len = 0
-        
-        for right in range(len(nums)):
-            window_count[nums[right]] += 1
-            
-            while window_count[0] > k:
-                window_count[nums[left]] -= 1
-                left += 1
-            
-            max_len = max(max_len, right - left + 1)
-        
-        return max_len"""

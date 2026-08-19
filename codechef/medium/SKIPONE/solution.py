4368ffe44eac0c793267046d7d82a3b058f69b52
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-19 21:48:03 +0530

    ✅ SKIPONE | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/SKIPONE/solution.py
+++ codechef/medium/SKIPONE/solution.py
@@ -1,1 +1,20 @@
 # cook your dish here
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    
+    total_sum = 0
+    max_seen = 0
+    answer = 0
+    
+    for i in range(N):
+        total_sum += A[i]
+        max_seen = max(max_seen, A[i])
+        
+        if total_sum - max_seen <= K:
+            answer = i + 1
+        else:
+            break
+    
+    print(answer)

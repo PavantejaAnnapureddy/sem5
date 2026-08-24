ba226c05d5f2e7a2369ab391e6f5b735cc7d4541
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-24 16:08:05 +0530

    ✅ Trapping Rain Water | Hard | Python | Runtime: 15 ms (37.85%) | Memory: 13.5 MB | 2026-08-24

---
+++ leetcode/hard/0042-trapping-rain-water/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        stack = []
+        water = 0
+        
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                
+                if not stack:
+                    break
+                
+                distance = i - stack[-1] - 1
+                bounded_height = min(height[i], height[stack[-1]]) - height[top]
+                water += distance * bounded_height
+            
+            stack.append(i)
+        
+        return water

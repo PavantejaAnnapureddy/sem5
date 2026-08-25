616811de35f3fe5033e94904d1156b9446646adc
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-25 14:34:41 +0530

    ✅ Corporate Flight Bookings | Medium | Python | Runtime: 25 ms (77.12%) | Memory: 22.5 MB | 2026-08-25

---
+++ leetcode/medium/1109-corporate-flight-bookings/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def corpFlightBookings(self, bookings, n):
+        """
+        :type bookings: List[List[int]]
+        :type n: int
+        :rtype: List[int]
+        """
+        answer = [0] * ( n + 2)
+        for first,last, seats in bookings:
+         answer[first]+= seats
+         answer[last +1]-= seats
+        for i in range(1,n+1):
+         answer[i] += answer[i - 1]
+
+        return  answer[1:n+1]
+
+        

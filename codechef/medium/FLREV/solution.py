f329f84da668383503a5757f7fab2c43897ddfa3
Pavan teja <24b11ds008@adityauniversity.in>
2026-08-26 20:11:30 +0530

    ✅ FLREV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/FLREV/solution.py
+++ codechef/medium/FLREV/solution.py
@@ -1,1 +1,19 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    
+    initial = 0
+    for i in range(n - 1):
+        if s[i] == s[i + 1]:
+            initial += 1
+    
+    max_beauty = initial
+    
+    for i in range(n - 1):
+        if s[i] != s[i + 1]:
+            max_beauty += 1
+            break
+    
+    print(min(max_beauty, n - 1))

d4e9f790f35d96e6c87489b76ac1f55727359e38
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-02 21:01:17 +0530

    ✅ MAXSUM77 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.py
+++ codechef/medium/MAXSUM77/solution.py
@@ -1,1 +1,17 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    
+    prefix = [0] * (n + 1)
+    for i in range(n):
+        prefix[i+1] = prefix[i] + arr[i]
+    
+    L = n - k
+    max_sum = 0
+    for i in range(k + 1):
+        curr_sum = prefix[i + L] - prefix[i]
+        max_sum = max(max_sum, curr_sum)
+    
+    print(max_sum)

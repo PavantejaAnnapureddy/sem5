ed7762c8d5244bbfe984617d527fb2eac005bb1e
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-02 21:06:02 +0530

    ✅ GOODSUBSETEZ | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/GOODSUBSETEZ/solution.py
+++ codechef/medium/GOODSUBSETEZ/solution.py
@@ -1,1 +1,12 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    freq = {}
+    for num in arr:
+        msb = num.bit_length()  
+        freq[msb] = freq.get(msb, 0) + 1
+    
+    print(max(freq.values()))

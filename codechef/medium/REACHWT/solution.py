4a9f554ed5da1054c1adcb1b92a7b400619ba90a
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-02 20:58:02 +0530

    ✅ REACHWT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/REACHWT/solution.py
+++ codechef/medium/REACHWT/solution.py
@@ -1,6 +1,11 @@
 # cook your dish here
 y = int(input())
-while in y:
+for _ in range(y):
     x=int(input())
+    if x % 2 == 0:
+        cost = (x // 2) * 30
+    else:
+        cost = (x // 2) * 30 + 20
+    print(cost)
     
     

56e003604da41775064dd12720ff5692fda5180a
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-02 21:14:10 +0530

    ✅ THREEGAME | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/THREEGAME/solution.py
+++ codechef/medium/THREEGAME/solution.py
@@ -1,10 +1,7 @@
-
 t = int(input())
 for _ in range(t):
     n = int(input())
     if n == 1:
         print(1)
-    elif n % 3 == 2:
-        print(n + 2)
     else:
-        print(n + 1)
+        print(3 * ((n + 2) // 3) + 1)

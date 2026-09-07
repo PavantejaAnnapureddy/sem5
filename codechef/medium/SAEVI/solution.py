59d972397af694544b9584b4ffa0072d789f0fdd
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-07 20:08:09 +0530

    ✅ SAEVI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/SAEVI/solution.py
+++ codechef/medium/SAEVI/solution.py
@@ -1,1 +1,12 @@
 # cook your dish here
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+threshold = 2 * K
+sum_value = 0
+
+for i in range(0, N, 2):  # step by 2 to get even indices
+    if A[i] > threshold:
+        sum_value += A[i]
+
+print(sum_value)

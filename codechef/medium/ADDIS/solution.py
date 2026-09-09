94453b45ec6b8844333f48a85864b0f33c8290f6
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-09 20:19:34 +0530

    ✅ ADDIS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/ADDIS/solution.py
+++ codechef/medium/ADDIS/solution.py
@@ -1,1 +1,13 @@
 # cook your dish here
+import math
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    
+    from collections import Counter
+    freq = Counter(A)
+    max_freq = max(freq.values())
+    
+    print((max_freq + 1) // 2)  

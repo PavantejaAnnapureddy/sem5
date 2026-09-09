39c8c24a3d0f43fb27cfc33ac15c9d6cd14b9c25
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-09 20:36:01 +0530

    ✅ BTCMINE | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/BTCMINE/solution.py
+++ codechef/medium/BTCMINE/solution.py
@@ -1,1 +1,13 @@
-# cook your dish here
+
+T = int(input())
+for _ in range(T):
+    X, Y = map(int, input().split())
+    days = 1
+    while True:
+        sum_squares = days * (days + 1) * (2 * days + 1) // 6
+        profit = Y * sum_squares - days * X
+        
+        if profit > 0:
+            print(days)
+            break
+        days += 1

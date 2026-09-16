fcde74b131a318dd49fe71e7cd656aa4c1d87c26
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-16 21:25:39 +0530

    ✅ FIXEDPTS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FIXEDPTS/solution.py
+++ codechef/medium/FIXEDPTS/solution.py
@@ -1,1 +1,10 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    if k == n:
+        print("Yes")
+    elif k <= n - 2:
+        print("Yes")
+    else:
+        print("No")

f219fd07fe9b039ffca3c02bf2cb55e28246712c
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-23 20:52:13 +0530

    ✅ LITUP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LITUP/solution.py
+++ codechef/medium/LITUP/solution.py
@@ -1,1 +1,19 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    c = list(map(int, input().split()))
+    
+    min_cost = float('inf')
+    
+    for i in range(n):
+        for j in range(i+1, n):
+            L_i = max(0, i-k)
+            R_i = min(n-1, i+k)
+            L_j = max(0, j-k)
+            R_j = min(n-1, j+k)
+            
+            if L_i == 0 and R_j == n-1 and R_i + 1 >= L_j:
+                min_cost = min(min_cost, c[i] + c[j])
+    
+    print(-1 if min_cost == float('inf') else min_cost)

7380d7a5bdf743580d9edeeaeddb30c70618862e
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-23 20:51:03 +0530

    ✅ PGNATK | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.py
+++ codechef/medium/PGNATK/solution.py
@@ -1,1 +1,12 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    if n <= k - 1:
+        print(n)
+    else:
+        q, r = divmod(n, k - 1)
+        if r == 0:
+            print(q * k - 1)
+        else:
+            print(q * k + r)

1e52447fad3a3f56e3188ebe8826af0f1ea53a34
Pavan teja <24b11ds008@adityauniversity.in>
2026-09-23 20:59:18 +0530

    ✅ FALLPR | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/FALLPR/solution.py
+++ codechef/medium/FALLPR/solution.py
@@ -1,1 +1,32 @@
 # cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    running_sum = 0
+    already_good = True
+    for x in a:
+        running_sum += x
+        if running_sum < 0:
+            already_good = False
+            break
+    
+    if already_good:
+        print("YES")
+        continue
+    found = False
+    for skip in range(n):
+        running_sum = 0
+        good = True
+        for i in range(n):
+            if i == skip:
+                continue
+            running_sum += a[i]
+            if running_sum < 0:
+                good = False
+                break
+        if good:
+            found = True
+            break
+    
+    print("YES" if found else "NO")
